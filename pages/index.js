ca3c86738e4cb1c862902eced0a461362ab1d247
russtack <stirlingest@gmail.com>
2023-10-28 06:55:37 -0400

    j

--- pages/index.js
+++ pages/index.js
@@ -87,12 +87,12 @@ export default function Home() {
         <section>
           <div className="lg:flex gap-10 dark:text-white">
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-800">
-              <Image
+              {/* <Image
                 src={design}
                 width={100}
                 height={100}
                 className="mx-auto"
-              />
+              /> */}
               <h3 className="text-lg font-medium pt-8 pb-2">
                 Front-end development
               </h3>
